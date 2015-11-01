d3b7869c0fe28390d2aa41fe53b63c5ff6e68a4c
Decha Moungsri <mamegomaday@gmail.com>
2015-11-01 15:43:21 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// import module
+var express = require('express');
+
+// intitialise module
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+// Create a route handle for the home path
+app.get('/', function(req,res){
+	// Send text back
+	//res.send('Hello World');
+
+	res.render('index', {
+		title: 'My Page'
+	});
+});
+
+app.listen(3000);
+console.log('Server listening at localhost:3000');
